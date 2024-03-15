06e53b400f2d16c31e226186653860ea329cfeae
tonymushah <tonyramanantsalama@gmail.com>
2024-03-15 12:17:03 +0300

    extract test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use csv::Reader;
+use data_analyse_01::structs::comandes_data_item::CommandesDataItem;
+use std::{fs::File, io::BufReader};
+
+fn main() -> anyhow::Result<()> {
+    let file = BufReader::new(File::open("./commandes_data.csv")?);
+    let mut reader = Reader::from_reader(file);
+    let datas = CommandesDataItem::from_csv_reader(&mut reader);
+    println!("{:#?}", datas);
+    Ok(())
 }
