5af0503a75b98746dd2c373b5d9e99de7829366b
tonymushah <tonyramanantsalama@gmail.com>
2024-03-15 13:12:31 +0300

    added data extraction

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use csv::Reader;
+use csv::ReaderBuilder;
 use data_analyse_01::structs::comandes_data_item::CommandesDataItem;
 use std::{fs::File, io::BufReader};
 
 fn main() -> anyhow::Result<()> {
     let file = BufReader::new(File::open("./commandes_data.csv")?);
-    let mut reader = Reader::from_reader(file);
-    let datas = CommandesDataItem::from_csv_reader(&mut reader);
+    let mut reader = ReaderBuilder::new().delimiter(b';').from_reader(file);
+    let datas = CommandesDataItem::from_csv_reader(&mut reader)?;
     println!("{:#?}", datas);
     Ok(())
 }
--- src/structs/comandes_data_item.rs
+++ src/structs/comandes_data_item.rs
@@ -1,8 +1,8 @@
 use std::io::Read;
 
 use csv::Reader;
-use serde::{Deserialize, Serialize};
-use time::Date;
+use serde::{de::Visitor, Deserialize, Deserializer, Serialize};
+use time::{macros::format_description, Date};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct CommandesDataItem {
@@ -10,14 +10,43 @@ pub struct CommandesDataItem {
     pub n_employe: usize,
     pub city: String,
     pub country: String,
+    #[serde(deserialize_with = "deserialize_date")]
     pub command_date: Date,
     pub n_command: usize,
+    #[serde(alias = "product_name")]
     pub product: String,
     pub total_price: String,
 }
 
+struct CommandesDataItemDateVisitor;
+
+impl<'de> Visitor<'de> for CommandesDataItemDateVisitor {
+    type Value = Date;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        writeln!(
+            formatter,
+            "Excepted string with format [day]/[month]/[year]"
+        )
+    }
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        let format = format_description!("[day]/[month]/[year]");
+        Date::parse(v, format).map_err(serde::de::Error::custom)
+    }
+}
+
+fn deserialize_date<'de, D>(deserializer: D) -> Result<Date, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    deserializer.deserialize_any(CommandesDataItemDateVisitor)
+}
+
 impl CommandesDataItem {
-    pub fn from_csv_reader<R: Read>(reader: &mut Reader<R>) -> Vec<Self> {
-        reader.deserialize::<Self>().flatten().collect()
+    pub fn from_csv_reader<R: Read>(reader: &mut Reader<R>) -> csv::Result<Vec<Self>> {
+        reader.deserialize::<Self>().collect()
     }
 }
